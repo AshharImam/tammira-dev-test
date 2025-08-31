f6b60efef7105769c29ce4278298858137a4c0cc
Ashhar Imam <ashharimam490@gmail.com>
2025-09-01 03:00:18 +0500

    Refactor code structure for improved readability and maintainability

--- app/src/assets/index.js
+++ app/src/assets/index.js
@@ -1,3 +1,11 @@
 export default {
     Search: require('./Search.png'),
+    Cross: require('./Cross.png'),
+    Plus: require('./Plus.png'),
+    FormatSize: require('./FormatSize.png'),
+    Share: require('./Share.png'),
+    Article: require('./Article.png'),
+    Warning: require('./Warning.png'),
+    Filter: require('./Filter.png'),
+    FilterOff: require('./FilterOff.png'),
 }
